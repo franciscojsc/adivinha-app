41f6a4d9a7df23155438b279c8f4605b5a5b9cb7
Francisco Chaves <francisco.jschaves@gmail.com>
2019-04-19 16:27:18 -0300

    Gerando número ao clicar

--- app/src/main/java/br/com/franciscochaves/adivinha/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/adivinha/MainActivity.java
@@ -2,12 +2,38 @@ package br.com.franciscochaves.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+    private Random random;
+    private int valor = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.button);
+        textoResultado = findViewById(R.id.textView3);
+
+        random = new Random();
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int valor = random.nextInt(11);
+                textoResultado.setText(valor+"");
+            }
+        });
+
     }
+
+
 }
